80c81449e0415a288ae3292382789893ea52f232
Daniel Boline <ddboline@gmail.com>
2021-06-10 06:53:55 -0400

    upgrade deadpool and fix unit tests

--- postgres_query/tests/deadpool.rs
+++ postgres_query/tests/deadpool.rs
@@ -1,12 +1,28 @@
 #![cfg(feature = "deadpool")]
 
-use postgres_query::*;
+use postgres_query::{Query, query};
 use deadpool_postgres::{Pool, Client, Config};
+use std::env;
 
 fn connect() -> Pool {
-    let mut cfg = Config::new();
-    cfg.dbname = Some("postgres_query_test".to_string());
-    cfg.host = Some("localhost".to_string());
+    let pgconf: tokio_postgres::Config = env::var("POSTGRES_DB_CONFIG")
+        .unwrap_or_else(|_| "user=postgres_query_test host=localhost".to_owned())
+        .parse().expect("Failed to open string");
+    let mut cfg = Config::default();
+    if let tokio_postgres::config::Host::Tcp(s) = &pgconf.get_hosts()[0] {
+        cfg.host.replace(s.to_string());
+    }
+    if let Some(u) = pgconf.get_user() {
+        cfg.user.replace(u.to_string());
+    }
+    if let Some(p) = pgconf.get_password() {
+        cfg
+            .password
+            .replace(String::from_utf8_lossy(p).to_string());
+    }
+    if let Some(db) = pgconf.get_dbname() {
+        cfg.dbname.replace(db.to_string());
+    }
     cfg.create_pool(tokio_postgres::NoTls).unwrap()
 }
 
@@ -14,6 +30,7 @@ fn connect() -> Pool {
 async fn simple_query() {
     let pool = connect();
     let client: Client = pool.get().await.unwrap();
-    let query: Query = query_dyn!("SELECT 14").unwrap();
-    let res = query.fetch_one::<(i32,), _>(&client).await;
+    let query: Query = query!("SELECT 14");
+    let res = query.fetch_one::<(i32,), _>(&client).await.expect("Failure");
+    assert_eq!(res, (14,));
 }
